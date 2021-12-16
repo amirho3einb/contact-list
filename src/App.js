5318ec19ea1205eb251cf4a0238ecd3f4e8b9cd3
Amirhossein <amirhossinbalagar@gmail.com>
2021-12-16 14:04:09 +0330

    contact list app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddContact from './components/AddContact/AddContact';
 
 function App() {
+  
+  const [contacts, setContacts] = useState([]);
+  
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, { id: Math.random() * 100, ...contact }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Contact App</h1>
+      <AddContact addContactHandler={addContactHandler} />
+      <section>Contact list</section>
+    </main>
   );
 }
 
