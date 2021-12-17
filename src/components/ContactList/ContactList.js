ff5e1f4c49cb4759be15bf35d9d6ceb185551283
Amirhossein <amirhossinbalagar@gmail.com>
2021-12-17 21:48:36 +0330

    add search to app

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -6,10 +6,13 @@ import { deleteContacts } from '../../services/deleteContactService';
 
 const ContactList = () => {
     const [contacts, setContacts] = useState(null);
+    const [allContacts, setAllContacts] = useState(null);
+    const [searrchTerm, setSearrchTerm] = useState("");
     useEffect(()=>{
         const getContacts = async () => {
           const { data } = await getALLContacts();
           setContacts(data);
+          setAllContacts(data);
         };
         try {
           getContacts();
@@ -24,8 +27,27 @@ const ContactList = () => {
         }
         catch(error){}
     }
+
+    const searchHandler = (e) => {
+        setSearrchTerm(e.target.value);
+        // filter contacts
+        const search = e.target.value;
+        if(search !== ""){
+            const filteredContacts = allContacts.filter( c => {
+                return Object.values(c)
+                .join(" ")
+                .toLowerCase()
+                .includes(search.toLowerCase());
+            });
+            setContacts(filteredContacts);
+        }
+        else{
+            setContacts(allContacts);
+        }
+    }
     return ( 
         <>
+            <div><input type="text" value={searrchTerm} onChange={searchHandler} placeholder='Search contact ...'/></div>
             {contacts ? contacts.map((contact) => {
                 return(
                     <Contact contact={contact} onDelete={deleteContractHandler} key={contact.id}/>
